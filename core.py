3fdebc837fce7fe21b925102e01a335295cdcdd2
Christopher <privateerchris@gmail.com>
2022-03-26 14:04:17 -1000

    reinforced check config

--- core.py
+++ core.py
@@ -1,6 +1,7 @@
 """
 Provides utility functions to maintain the current days in the file.
 """
+#pylint: disable=unspecified-encoding
 import datetime as dt
 from pathlib import Path
 
@@ -9,6 +10,18 @@ import yaml
 
 CFG_PATH = Path('config.yml')
 DAYS = {}
+_sheet_names = (
+    'Chris Food Plan',
+    "Melia's Food Plan",
+    "Bryn's Food Plan"
+)
+DEFAULTS = {
+    'names': {},
+    'sheets' : {name:None for name in _sheet_names},
+    'threaded':True,
+    'filename':'shopping_list',
+    'output_dir': '~/Desktop',
+}
 
 def build_days():
     """
@@ -24,21 +37,20 @@ def create_default_config():
     """
     Builds the default configuration file.
     """
-    default_names = (
-        'Chris Food Plan',
-        "Melia's Food Plan",
-        "Bryn's Food Plan")
-    yml_dict = {
-        'names': {},
-        'sheets' : {name:None for name in default_names},
-        'threaded':True,
-    }
     with open(CFG_PATH, 'w') as y_file:
-        yaml.dump(yml_dict, y_file)
+        yaml.dump(DEFAULTS, y_file)
 
 def check_config():
     """Verifies the config is ok to use."""
-    if not CFG_PATH.exists():
+    if CFG_PATH.exists():
+        with open(CFG_PATH, 'rb') as y_file:
+            yml_dict = yaml.load(y_file, yaml.Loader)
+        for key, default_val in DEFAULTS.items():
+            if key not in yml_dict:
+                yml_dict[key] = default_val
+        with open(CFG_PATH, 'w') as y_file:
+            yaml.dump(yml_dict, y_file, yaml.Dumper)
+    else:
         create_default_config()
 
 check_config()
